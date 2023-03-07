afc00d1901d703fd374bb160a25ed3a36c2658e3
Alex Alegre <6503910+Saityi@users.noreply.github.com>
2023-03-07 15:42:00 -0600

    Add a couple more unit tests of helper functions, as well as module-wide tests via stock_and_flow macro tests

--- test/Syntax.jl
+++ test/Syntax.jl
@@ -1,5 +1,7 @@
 using Test
-using StockFlow.Syntax: is_binop, sum_variables, infix_expression_to_binops
+using StockFlow
+using StockFlow.Syntax
+using StockFlow.Syntax: is_binop, sum_variables, infix_expression_to_binops, fnone_value_or_vector, extract_function_name_and_args_expr
 
 @testset "is_binop recognises binops" begin
     @test is_binop(:(a + b))
@@ -23,6 +25,13 @@ end
     @test infix_expression_to_binops(:(a + b))[1][1][2] == :(a + b)
 end
 
+@testset "infix_expression_to_binops creates right number of expressions" begin
+    @test length(infix_expression_to_binops(:(a + b + c))[1]) == 2
+    @test length(infix_expression_to_binops(:(a + b + c + d))[1]) == 3
+    @test length(infix_expression_to_binops(:(a + b + c + d + e))[1]) == 4
+    @test length(infix_expression_to_binops(:(a + b + c + d + e + f))[1]) == 5
+end
+
 @testset "infix_expression_to_binops throws exception when no binops provided" begin
     @test_throws Exception infix_expression_to_binops(:(f()))
     @test_throws Exception infix_expression_to_binops(:(f(a)))
@@ -31,4 +40,97 @@ end
 @testset "infix_expression_to_binops uses final symbol" begin
     @test infix_expression_to_binops(:(f(a, b)); finalsym=:testsym)[2] == :testsym
     @test infix_expression_to_binops(:(a + b); finalsym=:testsym)[2] == :testsym
+end
+
+@testset "fnone_value_or_vector" begin
+    empty_symbol_vector::Vector{Symbol} = []
+    @test fnone_value_or_vector(empty_symbol_vector) == :F_NONE
+    @test fnone_value_or_vector([:a]) == :a
+    @test fnone_value_or_vector([:a, :b]) == [:a, :b]
+end
+
+@testset "extract_function_name_args_expr extracts the flow name and flow definition" begin
+    @test extract_function_name_and_args_expr(:(testf(a))) == (:testf, :a)
+    @test extract_function_name_and_args_expr(:(testf(a + b + c + d))) == (:testf, :(a + b + c + d))
+end
+
+@testset "extract_function_name_args_expr rejects invalid flow expressions" begin
+    # Undefined flow equation
+    @test_throws Exception extract_function_name_and_args_expr(:(testf()))
+    # Multiparameter flows
+    @test_throws Exception extract_function_name_and_args_expr(:(testf(a, b)))
+end
+
+@testset "stock_and_flow macro generates the expected StockAndFlowF representations" begin
+    SIR_1_via_macro = @stock_and_flow begin
+        :stocks
+        S
+        I
+        R
+
+        :parameters
+        c
+        beta
+        tRec
+
+        :dynamic_variables
+        v_prevalence = I / N
+        v_meanInfectiousContactsPerS = c * v_prevalence
+        v_perSIncidenceRate = beta * v_meanInfectiousContactsPerS
+        v_newInfections = S * v_perSIncidenceRate
+        v_newRecovery = I / tRec
+
+        :flows
+        S => inf(v_newInfections) => I
+        I => rec(v_newRecovery) => R
+
+        :sums
+        N = [S, I, R]
+    end
+
+    SIR_1_canonical = StockAndFlowF(
+        # stocks
+        (:S => (:F_NONE, :inf, :N), :I => (:inf, :rec, :N), :R => (:rec, :F_NONE, :N)),
+        # parameters
+        (:c, :beta, :tRec),
+        # dynamical variables
+        (:v_prevalence => ((:I, :N) => :/),
+            :v_meanInfectiousContactsPerS => ((:c, :v_prevalence) => :*),
+            :v_perSIncidenceRate => ((:beta, :v_meanInfectiousContactsPerS) => :*),
+            :v_newInfections => ((:S, :v_perSIncidenceRate) => :*),
+            :v_newRecovery => ((:I, :tRec) => :/),
+        ),
+        # flows
+        (:inf => :v_newInfections, :rec => :v_newRecovery),
+        # sum dynamical variables
+        (:N),
+    )
+    @test SIR_1_via_macro == SIR_1_canonical
+
+    SIR_2 = @stock_and_flow begin
+        :stocks
+        S
+        I
+        R
+
+        :parameters
+        c
+        beta
+        tRec
+
+        # We can leave out dynamic variables and let them be inferred from flows entirely!
+
+        :flows
+        S => inf(S * beta * (c * (I / N))) => I
+        I => rec(I / tRec) => R
+
+        :sums
+        N = [S, I, R]
+    end
+
+    # Although the variable names are different
+    # due to gensym, the models should structurally be the same.
+    for part in SIR_2.parts
+        @test SIR_2.parts[part] == SIR_1_canonical.parts[part]
+    end
 end
