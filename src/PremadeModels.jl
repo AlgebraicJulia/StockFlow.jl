43d9bb9eb0bfb9c3a796fbb616aacbe9dec1f923
Thomas Purdy <temporal.underscore@gmail.com>
2023-08-07 19:40:23 -0600

    Removed id flows and associated params and vars, removed a few comments and changed some spacing.

--- src/PremadeModels.jl
+++ src/PremadeModels.jl
@@ -36,8 +36,7 @@ seir_model = @stock_and_flow begin
         tlatent
         trecovery
         δ
-        c
-        rAge 
+        c 
         
 
         :dynamic_variables
@@ -48,9 +47,9 @@ seir_model = @stock_and_flow begin
 
         v_birth = μ * N
 
-        v_inf = E / tlatent # may be better to multiply as opposed to divide
+        v_inf = E / tlatent
 
-        v_rec = I / trecovery # may be better to multiply than divide
+        v_rec = I / trecovery 
 
 
         v_deathS = δ * S       
@@ -58,10 +57,6 @@ seir_model = @stock_and_flow begin
         v_deathI = δ * I
         v_deathR = δ * R
         
-        v_idS = rAge * S
-        v_idE = rAge * E
-        v_idI = rAge * I
-        v_idR = rAge * R
 
         :flows
         CLOUD => f_birth(v_birth) => S
@@ -72,11 +67,6 @@ seir_model = @stock_and_flow begin
         I => f_rec(v_rec) => R
         I => f_deathI(v_deathI) => CLOUD
         R => f_deathR(v_deathR) => CLOUD
-
-        S => f_idS(v_idS) => S
-        E => f_idE(v_idE) => E
-        I => f_idI(v_idI) => I       
-        R => f_idR(v_idR) => R
 
         :sums
         N = [S, E, I, R]
@@ -96,12 +86,8 @@ sis_model = @stock_and_flow begin
     β
     trec # 1 / trecovery.  This corresponds to σ.
     δ
-    rAgeS
-    rAgeI
     c
 
-
-    
       
     :dynamic_variables
     v_deathsX = δ * S
@@ -112,11 +98,8 @@ sis_model = @stock_and_flow begin
     v_perSIncidenceRate = β * v_meanInfectiousContactsPerS
     v_newIncidence = S * v_perSIncidenceRate
 
-    # v_newInfectious
     v_newRecovery = I * trec
     v_deathsI = I * δ
-    v_agingS = S * rAgeS
-    v_agingI = I * rAgeI 
 
     :flows
 
@@ -126,9 +109,6 @@ sis_model = @stock_and_flow begin
     I => f_newRecovery(v_newRecovery) => S
 
     I => f_deathsI(v_deathsI) => CLOUD
-    S => f_agingX(v_agingS) => S
-    I => f_agingI(v_agingI) => I
-
 
     :sums
     N = [S, I]
@@ -148,7 +128,6 @@ sir_model = @stock_and_flow begin
     c
     β
     rRec
-    rAge
     
     :dynamic_variables
     v_prevalence = NI / NS
@@ -156,16 +135,10 @@ sir_model = @stock_and_flow begin
     v_perSIncidenceRate = β * v_meanInfectiousContactsPerS
     v_newInfections = S * v_perSIncidenceRate
     v_newRecovery = I * rRec
-    v_idS = S * rAge
-    v_idI = I * rAge
-    v_idR = R * rAge
     
     :flows
-    S => f_idS(v_idS) => S
     S => f_inf(v_newInfections) => I
-    I => f_idI(v_idI) => I
     I => f_rec(v_newRecovery) => R
-    R => f_idR(v_idR) => R
     
     :sums
     N = [S, I, R]
@@ -190,10 +163,8 @@ svi_model = @stock_and_flow begin
     evaccine
     c
     β
-    rAge
 
     :dynamic_variables
-
     v_vacc = S * rvaccine 
     v_deathV = δ * V
     
@@ -203,19 +174,12 @@ svi_model = @stock_and_flow begin
     v_vaccineInfectionRate = V / evaccine # same thing as multiplying by complement
     v_perSIncidenceVaccinated = v_vaccineInfectionRate * v_perSIncidenceRate
 
-    v_idS = S * rAge
-    v_idV = V * rAge
-    v_idI = I * rAge
-    
 
     :flows
     S => f_vacc(v_vacc) => V
     V => f_deathV(v_deathV) => CLOUD
     V => f_infV(v_perSIncidenceVaccinated) => I 
 
-    S => f_idS(v_idS) => S
-    V => f_idV(v_idV) => V
-    I => f_idI(v_idI) => I
     
     :sums
     N = [S, V, I]
