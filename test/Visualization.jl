b79fdaf0cb469a1af522bb6e0fa49b95c449c155
Thomas Purdy <temporal.underscore@gmail.com>
2024-07-29 14:04:32 -0600

    Fix visualization tests

--- test/Visualization.jl
+++ test/Visualization.jl
@@ -1,7 +1,7 @@
 @testset "Causal Loop F" begin
   @testset "Graphing Empty Causal Loop F" begin
     empty = CausalLoopPM()
-    empty_graph = GraphSF(empty)
+    empty_graph = GraphCL(empty)
     @test isempty(empty_graph.stmts)
   end
 
@@ -10,7 +10,7 @@
 
   @testset "Graphing standard Causal Loop F" begin
     ABA = CausalLoopPM([:A, :B], [:A => :B, :B => :A], [POL_NEGATIVE, POL_POSITIVE])
-    ABA_graph = GraphSF(ABA)
+    ABA_graph = GraphCL(ABA)
     
     n1 = node_info(ABA_graph.stmts[1])
     n2 = node_info(ABA_graph.stmts[2])
@@ -23,7 +23,7 @@
 
 
     C = CausalLoopPM([:C], [:C => :C for _ in 1:2], [POL_NEGATIVE, POL_POSITIVE])
-    C_graph = GraphSF(C)
+    C_graph = GraphCL(C)
 
     c_n1 = node_info(C_graph.stmts[1])
 
@@ -34,7 +34,7 @@
     @test (c_n1 == ("n1", "C") && c_e2 == ("n1", "n1", "-") && c_e1 == ("n1", "n1", "+"))
 
     DE = CausalLoopPM([:D, :E], [:D => :E], [POL_POSITIVE])
-    DE_graph = GraphSF(DE ; schema = "C0")
+    DE_graph = GraphCL(DE ; schema = "C0")
 
     de_n1 = node_info(DE_graph.stmts[1])
     de_n2 = node_info(DE_graph.stmts[2])
@@ -53,30 +53,15 @@
     @test isempty(Graph_RB(CausalLoopPM()).stmts)
 
     C2 = CausalLoopPM([:C2], [:C2 => :C2 for _ in 1:5], [POL_NEGATIVE, POL_POSITIVE, POL_NEGATIVE, POL_POSITIVE, POL_NEGATIVE])
-  #   C2_RB = Graph_RB(C2)
+    C2_RB = Graph_RB(C2)
 
 
   #   # number nodes + 2 * number edges
-  #   C_nodes = [node_info(C2_RB.stmts[i]) for i in 1:11]
+     C_nodes = [node_info(C2_RB.stmts[i]) for i in 1:11]
   #   # number edges * 3 
-  #   C_edges = [(C2_RB.stmts[i].path[1].name, C2_RB.stmts[i].path[2].name) for i in 12:26]
+     C_edges = [(C2_RB.stmts[i].path[1].name, C2_RB.stmts[i].path[2].name) for i in 12:26]
 
-  #   @show C_nodes, C_edges
-  #   @test (C_nodes == [("n1", "C2"), 
-  #   ("n2", "+"), ("n3", "+"), ("n4", "-"), ("n5", "+"), ("n6", "-"),
-  #   ("n7", "B"), ("n8", "R"), ("n9", "±"), ("n10", "?"), ("n11", "0")]
-
-  #   && C_edges == [
-
-  #   ("n1", "n2"), ("n2", "n1"),
-  #   ("n1", "n3"), ("n3", "n1"),
-  #   ("n1", "n4"), ("n4", "n1"),
-  #   ("n1", "n5"), ("n5", "n1"), 
-  #   ("n1", "n6"), ("n6", "n1"),
-
-  #   ("n2", "n7"), ("n3", "n8"), ("n4", "n9"), ("n5", "n10"), ("n6", "n11")
-  #   ])
-
+     @test (C_nodes, C_edges) == ([("n1", "C2"), ("n2", "+"), ("n3", "+"), ("n4", "-"), ("n5", "-"), ("n6", "-"), ("n7", "R"), ("n8", "R"), ("n9", "B"), ("n10", "B"), ("n11", "B")], [("n1", "n2"), ("n2", "n1"), ("n1", "n3"), ("n3", "n1"), ("n1", "n4"), ("n4", "n1"), ("n1", "n5"), ("n5", "n1"), ("n1", "n6"), ("n6", "n1"), ("n2", "n7"), ("n3", "n8"), ("n4", "n9"), ("n5", "n10"), ("n6", "n11")])
 
   end
 
